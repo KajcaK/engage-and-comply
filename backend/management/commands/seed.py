18bbe693f4da45352bbe45d3a6ee4bcbd5e0f675
yahluo-dev <yarikvas2003@gmail.com>
2025-05-30 23:29:40 +0200

    feat(DocumentModel): Store the pdf in the dtabase

--- backend/management/commands/seed.py
+++ backend/management/commands/seed.py
@@ -3,6 +3,8 @@
 from django.core.management.base import BaseCommand, CommandError
 from backend.models import StudentUser, Document  # Replace with your actual model
 from django.contrib.auth import get_user_model
+import os
+from django.core.files import File
 
 class Command(BaseCommand):
     help = 'Seeds the database with initial data'
@@ -17,12 +19,32 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS('Seeded StudentUsers...'))
 
         # 2. Seed Documents
-        Document.objects.create(
-            title="PACT Methodology V3: Methodology for Calculating and Exchanging Cradle-to-Gate Product Carbon Footprints (PCFs)",
-            publish_date="2025-06-15", pdf_file="stub")
-        Document.objects.create(
-            title="GHG Protocol Standards (Corporate / Scope 3 / Product)",
-            publish_date="2023-03-18", pdf_file="stub")
+
+        self.stdout.write(self.style.SUCCESS('Seeding the Document model...'))
+        pdf_file_path = os.path.join('backend/fixtures', 'pact-methodology.pdf')  # Replace 'data' and 'sample.pdf'
+        try:
+            with open(pdf_file_path, 'rb') as pdf_file:
+                document = Document(
+                    title='PACT Methodology V3: Methodology for Calculating and Exchanging Cradle-to-Gate Product Carbon Footprints (PCFs)',
+                    publish_date="2025-06-15"
+                )
+                document.pdf_file.save('sample.pdf', File(pdf_file), save=True)
+
+                self.stdout.write(self.style.SUCCESS(f'Created document: {document.title}, Path:{pdf_file_path}'))
+
+        except FileNotFoundError:
+            self.stdout.write(self.style.ERROR(f'PDF file not found at: {pdf_file_path}'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'Error seeding document: {e}'))
+
+        self.stdout.write(self.style.SUCCESS('Document seeding complete!'))
+
+        #Document.objects.create(
+        #    title="PACT Methodology V3: Methodology for Calculating and Exchanging Cradle-to-Gate Product Carbon Footprints (PCFs)",
+        #    publish_date="2025-06-15")
+        #Document.objects.create(
+        #    title="GHG Protocol Standards (Corporate / Scope 3 / Product)",
+        #    publish_date="2023-03-18")
         self.stdout.write(self.style.SUCCESS('Seeded Documents...'))
 
         User = get_user_model()

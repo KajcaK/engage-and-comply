4e61f1dcf778b59cf4a54f500cf4a0c7ef551127
Karolína Dicková <kdickova.dev@gmail.com>
2025-05-30 16:15:39 +0200

    base app layout

--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -2,7 +2,7 @@ import StoryContainer from "../components/StoryContainer.jsx";
 import ChatContainer from "../components/ChatContainer.jsx";
 
 const AppLayout = () => (
-    <div className="d-flex min-vh-100 bg-light justify-content-evenly align-items-center p-3">
+    <div className="d-flex min-vh-100 bg-light justify-content-evenly align-items-center p-3 app-layout">
         <StoryContainer/>
         <ChatContainer/>
     </div>

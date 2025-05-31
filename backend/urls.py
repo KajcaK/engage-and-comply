df9120af972664e61f0d544a08849a4b4ca4d9fe
yahluo-dev <yarikvas2003@gmail.com>
2025-05-31 04:42:25 +0200

    feat(Urls): Add an endpoint for question generation

--- backend/urls.py
+++ backend/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+    path('documents/<int:document_id>/generate_questions/', views.generate_test_questions, name='generate_test_questions'),
 ]
